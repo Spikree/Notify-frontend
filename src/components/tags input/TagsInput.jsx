4f12ab06f54b5ec42da270fa61ad6d3f497be0b2
Avi <avimahalingpure10@gmail.com>
2024-07-08 14:34:20 +0530

    add max tag limit to the add note component

--- src/components/tags input/TagsInput.jsx
+++ src/components/tags input/TagsInput.jsx
@@ -4,13 +4,14 @@ import { MdAdd, MdClose } from 'react-icons/md';
 
 const TagsInput = (props) => {
     const [inputValue, setInputValue] = useState("");
+    const maxTags = 4;
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
     const addNewTag = () => {
-        if (inputValue.trim() !== "") { // Fixed condition to add only if there's input
+        if (inputValue.trim() !== "" && props.tags.length < maxTags) {
             props.setTags([...props.tags, inputValue.trim()]);
             setInputValue("");
         }
@@ -29,30 +30,36 @@ const TagsInput = (props) => {
     return (
         <div className='tags'>
             {props.tags.length > 0 && (
-                <div className='tag-container'> 
+                <div className='tag-container'>
                     {props.tags.map((tag, index) => (
                         <span className='tag' key={index}>
                             # {tag}
-                            <button className='gray-button remove-button' onClick={() => handleRemoveTag(tag)}> 
-                                <MdClose className='icons' /> 
+                            <button className='gray-button remove-button' onClick={() => handleRemoveTag(tag)}>
+                                <MdClose className='icons' />
                             </button>
                         </span>
                     ))}
                 </div>
             )}
 
-            <div className='input-container'> 
-                <input 
-                    type="text" 
-                    placeholder='Add tags' 
+            {props.tags.length >= maxTags && (
+                <p className="limit-message">Max tag limit reached!</p>
+            )}
+
+            <div className='input-container'>
+                <input
+                    type="text"
+                    placeholder='Add tags'
                     value={inputValue}
                     onChange={handleInputChange}
-                    onKeyDown={handleKeyDown} 
+                    onKeyDown={handleKeyDown}
+                    disabled={props.tags.length >= maxTags}
                 />
-                <button className='add-tags-button gray-button' onClick={addNewTag}>
+                <button className='add-tags-button gray-button' onClick={addNewTag} disabled={props.tags.length >= maxTags}>
                     <MdAdd className='icons' />
                 </button>
             </div>
+
         </div>
     );
 };
